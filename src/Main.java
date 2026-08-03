2c9924b09dd204c55a8280779ba419fae4a973f7
Vaimond <vaimond.shell@gmail.com>
2026-08-03 20:47:23 +0300

    HW6

--- src/Main.java
+++ src/Main.java
@@ -1,111 +1,92 @@
 public class Main {
     public static void main(String[] args) {
-        // Задача 1
-        int clientOS = 0;
+        // Task 1
+        System.out.println("Taks 1");
 
-        if (clientOS == 1) {
-            System.out.println("Установите версию приложения для Android по ссылке");
-        } else {
-            System.out.println("Установите версию приложения для iOS по ссылке");
+        for (int i = 1; i < 11; i++) {
+            System.out.println(i);
         }
 
-        // Задача 2
-        int clientDeviceYear = 2015;
+        // Task 2
 
-        if (clientOS == 1 && (clientDeviceYear < 2015)) {
-            System.out.println("Установите облегченную версию приложения для Android по ссылке");
-        } else if (clientOS == 1 && (clientDeviceYear >= 2015)) {
-            System.out.println("Установите версию приложения для Android по ссылке");
-        } else if (clientOS == 0 && (clientDeviceYear < 2015)) {
-            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
-        } else if (clientOS == 0 && (clientDeviceYear >= 2015)) {
-            System.out.println("Установите версию приложения для iOS по ссылке");
+        System.out.println("Taks 2");
+
+        for (int i = 10; i != 0; i--) {
+            System.out.println(i);
         }
 
-        // Задача 3
-        int year = 1984;
+        // Task 3
 
+        System.out.println("Taks 3");
 
-        if (year > 1584) {
-            if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
-                System.out.println("Это год является високосным");
-            } else {
-                System.out.println("Этот год не является високосным");    }}
-
-
-        /*
-        if (year >= 1584) {
-            if(((year - 1584) % 400) == 0) {
-                System.out.println("Это год является високосным");
-            } else if (year > 1584 && ((year - 1584) % 100) == 0) {
-                System.out.println("Этот год не является високосным");
-            } else if (((year - 1584) % 4) == 0) {
-                System.out.println("Это год является високосным");
-            } else {
-                System.out.println("Этот год не является високосным");
-            }
-        }  else {
-            System.out.println("Этот год не является високосным");
-        }
-        */
-
-        // Задача 4
-        int deliveryDistance = 95;
-        short deliveryTime = 1;
-
-        if (deliveryDistance > 100) {
-            System.out.println("Доставки нет");
-        } if (deliveryDistance <= 20) {
-            System.out.println("Потребуется дней " + deliveryTime);
-        } else if (deliveryDistance > 20 && deliveryDistance <= 60) {
-            System.out.println("Потребуется дней " + (deliveryTime + 1));
-        } else if (deliveryDistance > 60 && deliveryDistance <= 100) {
-            System.out.println("Потребуется дней " + (deliveryTime + 2));
+        for (int i = 0; i < 17; i = i + 2) {
+            System.out.println(i);
         }
 
-        // Задача 5
-        byte monthNumber = 6;
+        // Task 4
 
-        switch (monthNumber) {
-            case 1:
-                System.out.println("1-й месяц принадлежит к сезону зима");
-                break;
-            case 2:
-                System.out.println("2-й месяц принадлежит к сезону зима");
-                break;
-            case 3:
-                System.out.println("3-й месяц принадлежит к сезону весна");
-                break;
-            case 4:
-                System.out.println("4-й месяц принадлежит к сезону весна");
-                break;
-            case 5:
-                System.out.println("5-й месяц принадлежит к сезону весна");
-                break;
-            case 6:
-                System.out.println("6-й месяц принадлежит к сезону лето");
-                break;
-            case 7:
-                System.out.println("7-й месяц принадлежит к сезону лето");
-                break;
-            case 8:
-                System.out.println("8-й месяц принадлежит к сезону лето");
-                break;
-            case 9:
-                System.out.println("9-й месяц принадлежит к сезону осень");
-                break;
-            case 10:
-                System.out.println("10-й месяц принадлежит к сезону осень");
-                break;
-            case 11:
-                System.out.println("11-й месяц принадлежит к сезону осень");
-                break;
-            case 12:
-                System.out.println("12-й месяц принадлежит к сезону зима");
-                break;
-            default:
-                System.out.println("Номер месяца больше 12");
-                break;
+        System.out.println("Taks 4");
+
+        for (int i = 10; i != -10; i--) {
+            System.out.println(i);
+        }
+
+        // Task 5
+
+        System.out.println("Taks 5");
+
+        for (int i = 1904; i != 2096; i = i + 4) {
+            System.out.println(i + " год является високосным");
+        }
+
+        // Task 6
+
+        System.out.println("Taks 6");
+
+        for (int i = 7; i != 105; i = i + 7) {
+            System.out.println(i);
+        }
+
+        // Task 7
+
+        System.out.println("Taks 7");
+
+        for (int i = 1; i != 1024; i = i * 2) {
+            System.out.println(i);
+        }
+
+        // Task 8
+
+        System.out.println("Taks 8");
+
+        int salary = 29000;
+        int total = 0;
+
+        for (int i = 0; i < 12; i++) {
+            total = total + salary;
+            System.out.println("Месяц " + i + ", сумма накоплений равна " + total + " рублей");
+        }
+
+        // Task 9
+
+        System.out.println("Taks 8");
+
+        salary = 29000;
+        total = 0;
+
+        for (int i = 0; i < 12; i++) {
+            total = total + total/100;
+            total = total + salary;
+            System.out.println("Месяц " + i + ", сумма накоплений равна " + total + " рублей");
+        }
+
+        // Task 10
+
+        System.out.println("Taks 10");
+
+        for (int i = 1; i != 11; i++) {
+            int cons = 2;
+            System.out.println(cons + " * " + i + " = " + cons * i);
         }
     }
 }
