3f6e7fb755fc94b943faf15ca6bad0c07ef1475e
Vaimond <vaimond.shell@gmail.com>
2026-08-26 11:32:54 +0300

    HW9

--- src/Main.java
+++ src/Main.java
@@ -1,87 +1,121 @@
+import java.util.ArrayDeque;
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
         // Task 1
 
-        int[] array = new int[]{1, 2, 3};
+        System.out.println("Task 1");
 
-        double[] flo = new double[]{1.57, 7.654, 9.986};
+        int[] inputArray1 = {150942, 60456, 89546, 47565, 322679};
+        int sum = 0;
+        int maxPay = -1;
+        int minPay = 0;
+        float averagePay = 0.0f;
+
+        // Поиск суммы и максимальной выплаты
+        for (int element : inputArray1) {
+            sum = sum + element;
+            if (maxPay < element) {
+                maxPay = element;
+            }
+        }
+
+        // Расчёт среднего значения
+        averagePay = (float) sum / inputArray1.length;
+
+        // Поиск минимального значения
+        for (int i = 0; i < inputArray1.length - 1; i++) {
+            if (inputArray1[i] > inputArray1[i + 1]) {
+                minPay = inputArray1[i + 1];
+            }
+        }
+
+        float[] outputArray1 = {sum, maxPay, minPay, averagePay};
+
+        System.out.println(Arrays.toString(inputArray1));
+        System.out.println(Arrays.toString(outputArray1));
+
 
         // Task 2
 
         System.out.println("Task 2");
 
-        int i = 0;
-        while (i < 3) {
+        int[] inputArray2 = {150942, 60456, 89546, 47565, 322679};
+        float[] outputArray2 = new float[5];
+        float tax = 0.13f;
+        float payTax = 0.0f;
+        int count2 = 0;
 
-            if (i == 2) {
-                System.out.print(array[i]);
-                break;
-            }
-            System.out.print(array[i] + ", ");
-            i++;
-
+        for (float element : inputArray2) {
+            payTax = element * tax;
+            outputArray2[count2] = payTax;
+            count2++;
         }
 
-        System.out.println(" ");
 
-        i = 0;
-        while (i < 3) {
+        System.out.println(Arrays.toString(inputArray2));
+        System.out.println(Arrays.toString(outputArray2));
 
-            if (i == 2) {
-                System.out.print(flo[i]);
-                break;
-            }
-            System.out.print(flo[i] + ", ");
-            i++;
-        }
-
-        System.out.println(" ");
 
         // Task 3
 
         System.out.println("Task 3");
 
-        i = 2;
-        while (i != - 1) {
-            if (i == 0) {
-                System.out.print(array[i]);
-                break;
+        int[] inputArray3 = {1000, 2500, 5000, 7800, 13200};
+        boolean[] outputArray3 = new boolean[5];
+        int templateBonus = 5000;
+        int count3 = 0;
+
+        for (int element : inputArray3) {
+            if (element < templateBonus) {
+                outputArray3[count3] = false;
+                count3++;
+            } else {
+                outputArray3[count3] = true;
+                count3++;
             }
-            System.out.print(array[i] + ", ");
-            i--;
         }
 
-
-        System.out.println(" ");
-
-        i = 2;
-        while (i != - 1) {
-            if (i == 0) {
-                System.out.print(flo[i]);
-                break;
-            }
-            System.out.print(flo[i] + ", ");
-            i--;
-        }
-
-        System.out.println(" ");
+        System.out.println(Arrays.toString(inputArray3));
+        System.out.println(Arrays.toString(outputArray3));
 
         // Task 4
 
         System.out.println("Task 4");
 
-        i = 0;
+        int[] inputArray4 = {1000, 2500, -5000, 7800, -13200};
+        boolean[] outputArray4 = new boolean[1];
 
-        while (i < array.length) {
-            if (array[i] % 2 != 0){
-                array[i] = array[i] + 1;
-                i++;
+        for (int element : inputArray4) {
+            if (element < 0) {
+                outputArray4[0] = false;
+                break;
+            } else {
+                outputArray4[0] = true;
             }
-            i++;
         }
-        System.out.println(Arrays.toString(array));
 
+        System.out.println(Arrays.toString(inputArray4));
+        System.out.println((Arrays.toString(outputArray4)));
+
+        // Task 5
+
+        System.out.println("Task 5");
+
+        int[] inputArray5 = {415765, 518276, 781492, 641328, -9851763};
+        int[] outputArray5 = new int[1];
+        int count = 0;
+
+        for (int element : inputArray5) {
+            if (element > 0) {
+                count++;
+            }
+        }
+
+        outputArray5[0] = count;
+
+        System.out.println(Arrays.toString(inputArray5));
+        System.out.println(Arrays.toString(outputArray5));
     }
 }
